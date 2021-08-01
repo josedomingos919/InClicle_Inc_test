23f7caf4fb2d778481f58123c6030cfb935e53b6
josedomingos919 <josedomingos919@gmail.com>
2021-08-01 10:16:32 +0100

    fim

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import "./App.css";
-import userImg from "./assets/img/jose.jpg";
-import locationIcon from "./assets/icons/current-location-regular-24.png";
-import localImage1 from "./assets/icons/city-solid-24.png";
-import localImage2 from "./assets/icons/city-solid-27.png";
+import {
+  user,
+  cardInfo,
+  imgPending,
+  imgUser,
+  imgVideo,
+  imgPublic,
+  imgIndicator,
+  imgLocation,
+  imgPhoto,
+  imgLock,
+  imgCake,
+  imgRelogio,
+  imgDowArro,
+  imgUser1,
+  imgShare,
+  imgComment,
+  imgLike,
+  imgLike_2,
+} from "./util";
+
+import { useState } from "react";
 
 function App() {
-  const user = {
-    name: "kenedy Morais",
-    img: userImg,
-    email: "@Kenedymorais",
-    skill: "Front-End Developer",
-    location: "Rio Branco/Acre - Brasil",
-    local: [
-      {
-        img: localImage1,
-        text: "InCicle",
-      },
-      {
-        img: localImage2,
-        text: "Uninorte",
-      },
-    ],
+  const [post, setPost] = useState([
+    {
+      name: "Patricia Santos",
+      img: imgUser1,
+      message: 'Essa publicação está configurada no modo privado "somente eu"',
+      like: false,
+      comments: 0,
+      shares: 0,
+    },
+    {
+      name: "Patricia Santos",
+      img: imgUser,
+      message: "Hoje eu vou a praia e levarei a Ana comigo",
+      like: true,
+      comments: 8,
+      shares: 7,
+    },
+  ]);
+
+  const handleChangePost = (index, isLike, isOther) => {
+    const copy = [...post];
+
+    if (isLike) {
+      copy[index].like = !post[index].like;
+    } else if (isOther) {
+      copy[index][isOther]++;
+    }
+
+    setPost(copy);
   };
 
   return (
@@ -39,7 +70,7 @@ function App() {
             </div>
             <div className="card-footer">
               <div className="div-location">
-                <img src={locationIcon} alt="" />
+                <img src={imgLocation} alt="" />
                 <label> {user.location} </label>
               </div>
 
@@ -58,8 +89,199 @@ function App() {
         </div>
       </section>
 
-      <section className="sec-center">center</section>
-      <section className="sec-right">right</section>
+      <section className="sec-center">
+        <div className="div-public">
+          <div className="div-public-main">
+            <div className="public-icon-right">
+              <img className="img-icon-public" src={imgPublic} alt="" />
+            </div>
+            <div className="text-div-public-1"> Escreva uma publicação... </div>
+            <div className="public-icon-right">
+              <img className="img-icon-public" src={imgPhoto} alt="" />
+            </div>
+            <div className="public-icon-right">
+              <img className="img-icon-public" src={imgVideo} alt="" />
+            </div>
+          </div>
+          <div className="div-footer-public">
+            <p>
+              ou <a href="http://localhost:3000"> publique um artigo </a> na
+              InClicle
+            </p>
+          </div>
+        </div>
+
+        <div className="div-container-post">
+          {post.map(({ name, img, message, comments, shares, like }, index) => (
+            <div key={index} className="div-post">
+              <div className="div-header-post">
+                <div className="img-user post">
+                  <img className="img-user-d post" src={img} alt="" />
+                </div>
+                <div className="div-post-info">
+                  <h1 className="h-title-post">{name}</h1>
+                  <p>
+                    <img className="img-post-icon" src={imgRelogio} alt="" />
+                    {"  "}há cerca de 8 horas{" "}
+                    <img className="img-post-icon" src={imgLock} alt="" />{" "}
+                    Somente eu
+                  </p>
+                </div>
+                <div>
+                  <img className="img-post-icon" src={imgDowArro} alt="" />
+                </div>
+              </div>
+              <div className="div-post-message">
+                <p>{message}</p>
+              </div>
+
+              <div className="div-post-coment">
+                <div className="div-first-h">
+                  <label className="lbl-like-post-1">
+                    <img className="img-post-icon t16" src={imgLike} alt="" />{" "}
+                  </label>
+                  <label>
+                    {like ? (
+                      <p>1 Pessoa curtiu isso</p>
+                    ) : (
+                      <p>Seja o primeiro a curtir</p>
+                    )}
+                  </label>
+                </div>
+
+                <div className="div-info-share">
+                  <div>
+                    <p>{comments} comentários</p>
+                  </div>
+                  <div>
+                    <p>{shares} compartilhamentos</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="div-btn-post-2">
+                <button
+                  className="btn-app"
+                  onClick={() => {
+                    handleChangePost(index, true);
+                  }}
+                >
+                  <label className="lbl-like-post-1">
+                    <img className="img-post-icon t16" src={imgLike_2} alt="" />{" "}
+                  </label>{" "}
+                  {!like ? "Curtir" : "Não Curtir"}
+                </button>
+
+                <button
+                  onClick={() => {
+                    handleChangePost(index, false, "comments");
+                  }}
+                  className="btn-app"
+                >
+                  <label className="lbl-like-post-1">
+                    <img
+                      className="img-post-icon t16"
+                      src={imgComment}
+                      alt=""
+                    />{" "}
+                  </label>{" "}
+                  Comentários
+                </button>
+                <button
+                  onClick={() => {
+                    handleChangePost(index, false, "shares");
+                  }}
+                  className="btn-app"
+                >
+                  <label className="lbl-like-post-1">
+                    <img className="img-post-icon t16" src={imgShare} alt="" />{" "}
+                  </label>{" "}
+                  Compartilhar
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="sec-right">
+        <div className="right-card-1">
+          <div className="div-indicator">
+            <img alt="" src={imgIndicator} /> <label>Eficiência</label>
+          </div>
+          <div className="div-second">
+            <div className="div-chart-1">
+              <div className="div-line-chart">
+                <div>
+                  <div>Atividades</div>
+                  <div>23/106</div>
+                </div>
+                <div>50%</div>
+              </div>
+
+              <div>
+                <div>
+                  <div>Atividades</div>
+                  <div>23/106</div>
+                </div>
+                <div>50%</div>
+              </div>
+            </div>
+
+            <div className="div-chart-2">
+              <div className="div-line-chart-2">
+                <div>
+                  <div>Atividades</div>
+                  <div>23/106</div>
+                </div>
+                <div className="div-line-percentag">50%</div>
+              </div>
+            </div>
+
+            <div className="div-square">
+              {cardInfo.map(({ text, data, borderColor }, key) => (
+                <div className="card-class" style={{ borderColor }} key={key}>
+                  <header>{text}</header>
+                  <div>
+                    <ul>
+                      {data.map(({ text, value }, key) => (
+                        <li className="lbl-elemnt" key={key}>
+                          <label>{text}</label>
+                          <label>{value}</label>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="right-card-1">
+          <div className="div-indicator">
+            <img alt="" src={imgCake} /> <label>Aniversário</label>
+          </div>
+          <div className="div-second">
+            <div className="div-chart-1"></div>
+            <div className="div-tex-brith-day">
+              <p>Nenhum dos seus amigos comemora aniversário hoje</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-card-1">
+          <div className="div-indicator">
+            <img alt="" src={imgPending} /> <label>Pendência</label>
+          </div>
+          <div className="div-second last">
+            <div className="div-date-pend">Seg, 21 de Jun</div>
+            <div className="div-tex-brith-day">
+              <p>Você não possui nenhuma pendência para hoje</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,112 @@
+import imgLocal1 from "./assets/icons/city-solid-24.png";
+import imgLocal2 from "./assets/icons/city-solid-27.png";
+import imgUser from "./assets/img/jose.jpg";
+import imgLocation from "./assets/icons/current-location-regular-24.png";
+import imgIndicator from "./assets/img/indicator.jpg";
+import imgPublic from "./assets/icons/icons8_Edit_Property_50px.png";
+import imgVideo from "./assets/icons/icons8_Video_Call_50px.png";
+import imgPhoto from "./assets/icons/icons8_Image_50px.png";
+import imgRelogio from "./assets/icons/icons8_Alarm_Clock_50px.png";
+import imgLock from "./assets/icons/icons8_Lock_50px.png";
+import imgDowArro from "./assets/icons/icons8_Expand_Arrow_50px.png";
+import imgLike from "./assets/icons/icons8_Facebook_Like_50px.png";
+import imgLike_2 from "./assets/icons/icons8_Facebook_Like_50px_1.png";
+import imgComment from "./assets/icons/icons8_SMS_50px.png";
+import imgShare from "./assets/icons/icons8_Share_3_50px.png";
+import imgUser1 from "./assets/img/rest full api.jpg";
+import imgCake from "./assets/img/icons8_Birthday_Cake_100px.png";
+import imgPending from "./assets/img/icons8_Data_Pending_100px.png";
+
+export const user = {
+  name: "kenedy Morais",
+  img: imgUser,
+  email: "@Kenedymorais",
+  skill: "Front-End Developer",
+  location: "Rio Branco/Acre - Brasil",
+  local: [
+    {
+      img: imgLocal1,
+      text: "InCicle",
+    },
+    {
+      img: imgLocal2,
+      text: "Uninorte",
+    },
+  ],
+};
+
+export const cardInfo = [
+  {
+    text: "Atividades",
+    data: [
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+    ],
+    borderColor: "#6698A1",
+  },
+  {
+    text: "Projetos",
+    data: [
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+    ],
+    borderColor: "#A2AB72",
+  },
+  {
+    text: "Atividades",
+    data: [
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+      {
+        text: "Pedente",
+        value: 3,
+      },
+    ],
+    borderColor: "#A8956D",
+  },
+];
+
+export {
+  imgPending,
+  imgCake,
+  imgVideo,
+  imgPublic,
+  imgIndicator,
+  imgLocation,
+  imgPhoto,
+  imgUser,
+  imgLock,
+  imgRelogio,
+  imgDowArro,
+  imgUser1,
+  imgShare,
+  imgComment,
+  imgLike_2,
+  imgLike,
+};
